aaab3f2d66b739cf0e1ef60685098e0a241ed5dd
Nikola <nikolapajak2706@gmail.com>
2024-04-07 19:52:11 +0200

    first commit

--- Company.java
+++ Company.java
@@ -4,25 +4,25 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
-        // Dodawanie pracowników
         employees[0] = new Manager("Michael Jackson", 60000, 0);
         employees[1] = new Worker("Harry Potter", 60000, "Deweloper");
         employees[2] = new Employee("Adam Ken", 71000);
         employees[3] = new Worker("Bob Smith", 63000, "Księgowy");
         employees[4] = new Worker("Santa White", 18000, "Stazysta");
+        employees[5] = new Manager("Barbie Ben", 50000, 0);
+        employees[6] = new Worker("Kim King", 77000, "Marketer");
 
-        int nonManagerCount = 0;
         for (Employee employee : employees) {
-            if (!(employee instanceof Manager)) {
-                nonManagerCount++;
+            employee.setSalary(employee.getSalary() + 500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2); // Ustawienie liczby podwładnych
+                employee.setSalary(75000); // Ustawienie nowej pensji dla managerów
             }
         }
-        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
-        employees[0].setSalary(75000);
 
-        System.out.println("Dane dla wszystkich pracowników:");
+        System.out.println("Zaktualizowane dane pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
