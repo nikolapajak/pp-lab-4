5a32eb07ea892e6042759a06d8b65ca6b36b7014
Nikola <nikolapajak2706@gmail.com>
2024-04-07 19:42:47 +0200

    zad3

--- Company.java
+++ Company.java
@@ -6,11 +6,21 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
+        // Dodawanie pracowników
         employees[0] = new Manager("Michael Jackson", 60000, 0);
         employees[1] = new Worker("Harry Potter", 60000, "Deweloper");
         employees[2] = new Employee("Adam Ken", 71000);
-        employees[3] = new Worker("Bob Smith", 63000, "Księgowa");
+        employees[3] = new Worker("Bob Smith", 63000, "Księgowy");
         employees[4] = new Worker("Santa White", 18000, "Stazysta");
+
+        int nonManagerCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(75000);
 
         System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
