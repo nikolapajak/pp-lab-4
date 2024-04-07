abbd0c88dbdb9abcfe67477ecef717a227d0d4a8
Nikola <nikolapajak2706@gmail.com>
2024-04-07 19:27:02 +0200

    zad2

--- employees/Employee.java
+++ employees/Employee.java
@@ -1,3 +1,5 @@
+package employees;
+
 public class Employee {
     private String fullName;
     private double salary;
@@ -7,7 +9,7 @@ public class Employee {
         this.salary = salary;
     }
 
-    public String getName() {
+    public String getFullName() {
         return fullName;
     }
 
@@ -21,6 +23,6 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Employee: " + fullName + ", Salary=" + salary;
+        return "Employee: " + fullName + ", Salary: " + salary;
     }
 }
